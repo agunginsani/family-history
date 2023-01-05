2f9f573057b89b7930c0e4f2ebdde07846e8cd9b
Agung Insani <agunginsanialam@gmail.com>
2023-01-05 22:42:22 +0700

    chore: log error

--- app/routes/__authenticated.tsx
+++ app/routes/__authenticated.tsx
@@ -35,6 +35,7 @@ export async function loader({ request }: LoaderArgs) {
     const menus = await getMenusByRole(user.roleId);
     return { name: user.name, initial: user.name.charAt(0), menus };
   } catch (error) {
+    console.error(error);
     throw redirect("/login", {
       headers: {
         "Set-Cookie": await destroySession(session),
